0ff48b56137cacff2db99ab4dac8526e03aa060f
Ata Fatahi <immrata@gmail.com>
2025-09-08 11:24:42 -0700

    complete async distill loop
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

--- distill_async.py
+++ distill_async.py
@@ -1,15 +1,30 @@
 import ray
-from slime.ray.placement_group import create_actor_group, create_placement_groups, create_rollout_manager
+from slime.ray.distillation.placement_group import create_placement_groups, create_student_group, create_teacher_engine
 from slime.utils.arguments import parse_args
-from slime.utils.wandb_utils import init_wandb_primary
 
 def distill_async(args):
     pgs = create_placement_groups(args)
-    wandb_run_id = init_wandb_primary(args)
 
-    student_model = create_actor_group(args, pgs["student"], wandb_run_id=wandb_run_id)
+    student_model = create_student_group(args, pgs["student"])
 
     # create the rollout manager, with sglang engines inside.
-    rollout_manager = create_rollout_manager(args, pgs["rollout"], wandb_run_id=wandb_run_id)
+    teacher_engine = create_teacher_engine(args, pgs["teacher"])
 
-    assert not args.offload and not args.colocate, "Offload and colocate are not supported for full async RL training."
+    ids = student_model.async_init(args)
+    assert len(set(ids)) == 1
+    train_step = 0
+    for train_step in range(args.num_train_steps):
+        rollout_data_ref = ray.get(teacher_engine.async_generate(train_step))
+        teacher_log_probs_ref = ray.get(teacher_engine.async_compute_log_probs(rollout_data_ref))
+
+        ray.get(student_model.async_distill(rollout_data_ref, teacher_log_probs_ref))
+        if train_step % args.save_interval == 0:
+            ray.get(student_model.async_save_model(train_step))
+
+        if args.eval_interval is not None and train_step % args.eval_interval == 0:
+            ray.get(student_model.async_eval(train_step))
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    distill_async(args)
